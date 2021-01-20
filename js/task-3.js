23e8a16679b032429167f8c46e4320d1422180c9
Olga Konopleva <olenenok.she@gmail.com>
2021-01-20 19:33:53 +0200

    Испаравляет ошибку во 2 задаче

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,23 @@
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
-  
+
   const makeTransaction = (transaction) => {
     const delay = randomIntegerFromInterval(200, 500);
-   
     return new Promise ((resolve, rejecte) => 
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
-        
+      
       if (canProcess) {
-        resolve(transaction.id, delay);
+        
+        resolve([transaction.id, delay]);
       } else {
         rejecte(transaction.id);
       }
     }, delay));
   };
   
-  const logSuccess = (id, time) => {
+  const logSuccess = ([id, time]) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
   
